bfa19d8d43318a52aec8897b4cc1420cafd3327f
AlbertoCombina <xaratorx@gmail.com>
2024-05-07 20:35:40 +0200

    Tested on real hardware

--- MasterNode/src/main.cpp
+++ MasterNode/src/main.cpp
@@ -1,13 +1,17 @@
 #include <Arduino.h>
 #include <master_node_pins.h>
-#include <dht_22.h>
+#include <MyDht_22.h>
 
-
+MyDht_22 inst_dht22 = MyDht_22(DHT_22_DATA_PIN , MyDht_22::tempType::CELSIUS);
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(test(DHT_22_DATA_PIN));
-  delay(5000);
+  static unsigned long currMillis = millis();
+  static uint8_t ret = 0;
+  if (millis() - currMillis > 5000 || ret != 0){
+    ret = inst_dht22.printTemp();
+    currMillis = millis();
+  }
 }
